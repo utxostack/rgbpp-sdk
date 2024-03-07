aa04831fa1070283cb61cd71c203dbf734e8d510
dylan <duanyytop@gmail.com>
2024-03-07 16:34:03 +0800

    feat: add L1 transfer constructor

---
+++ packages/ckb/src/rgbpp/l1-transfer.ts
@@ -0,0 +1,83 @@
+import { RgbppL1TransferVirtualParams, RgbppL1TransferVirtualResult, RgbppL1TransferVirtualTx } from '../types/rgbpp';
+import { blockchain } from '@ckb-lumos/base';
+import { NoRgbppLiveCellError } from '../error';
+import { append0x, calculateRgbppCellCapacity, u128ToLe, u32ToLe } from '../utils';
+import { calculateCommitment, genRgbppLockScript } from '../utils/rgbpp';
+import { IndexerCell } from '../types';
+import { getRgbppLockDep, getSecp256k1CellDep, getXudtDep } from '../constants';
+
+export const genTransferL1VirtualTx = async ({
+  collector,
+  xudtTypeBytes,
+  rgbppLockArgsList,
+  transferAmount,
+  isMainnet,
+}: RgbppL1TransferVirtualParams): Promise<RgbppL1TransferVirtualResult> => {
+  const xudtType = blockchain.Script.unpack(xudtTypeBytes) as CKBComponents.Script;
+
+  const rgbppLocks = rgbppLockArgsList.map((args) => genRgbppLockScript(args, isMainnet));
+  let rgbppCells: IndexerCell[] = [];
+  for await (const rgbppLock of rgbppLocks) {
+    const cells = await collector.getCells({ lock: rgbppLock, type: xudtType });
+    if (!cells || cells.length === 0) {
+      throw new NoRgbppLiveCellError('No rgb++ cells found with the xudt type script and the rgbpp lock args');
+    }
+    rgbppCells = [...rgbppCells, ...cells];
+  }
+
+  const {
+    inputs,
+    capacity: sumInputsCapacity,
+    amount: sumAmount,
+  } = collector.collectUdtInputs(rgbppCells, transferAmount);
+
+  const rpbppCellCapacity = calculateRgbppCellCapacity(xudtType);
+  const outputsData = [append0x(u128ToLe(transferAmount))];
+  const outputs: CKBComponents.CellOutput[] = [
+    {
+      lock: genRgbppLockScript(u32ToLe(1)),
+      type: xudtType,
+      capacity: append0x(rpbppCellCapacity.toString(16)),
+    },
+  ];
+
+  if (sumAmount > transferAmount) {
+    outputs.push({
+      lock: genRgbppLockScript(u32ToLe(2)),
+      type: xudtType,
+      capacity: append0x(rpbppCellCapacity.toString(16)),
+    });
+    outputsData.push(append0x(u128ToLe(sumAmount - transferAmount)));
+  }
+
+  const cellDeps = [getRgbppLockDep(isMainnet), getXudtDep(isMainnet)];
+  const needPaymasterCell = inputs.length < outputs.length;
+  if (needPaymasterCell) {
+    cellDeps.push(getSecp256k1CellDep(isMainnet));
+  }
+  const witnesses = inputs.map((_) => '0x');
+
+  const ckbRawTx: CKBComponents.RawTransaction = {
+    version: '0x0',
+    cellDeps,
+    headerDeps: [],
+    inputs,
+    outputs,
+    outputsData,
+    witnesses,
+  };
+
+  const virtualTx: RgbppL1TransferVirtualTx = {
+    inputs,
+    outputs,
+    outputsData,
+  };
+  const commitment = calculateCommitment(virtualTx);
+
+  return {
+    ckbRawTx,
+    commitment,
+    needPaymasterCell,
+    sumInputsCapacity,
+  };
+};
