00f7aaa2e3364ac9511646db636948bd7ccf9c94
Dawn <13477199767@163.com>
2024-03-07 11:03:39 +0800

    chore:Create a demo application for a Vite project

---
+++ apps/vite/vite.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [nodePolyfills()],
+});
