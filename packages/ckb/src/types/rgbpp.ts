d49c0b54287f56bb91c48f55ba8cc56f877316b6
dylan <duanyytop@gmail.com>
2024-03-07 16:30:45 +0800

    add rgbpp types

--- packages/ckb/src/types/rgbpp.ts
+++ packages/ckb/src/types/rgbpp.ts
@@ -1,7 +1,7 @@
 import { Collector } from '../collector';
 import { Address, Hex } from './common';
 
-export interface ConstructParams {
+export interface ConstructPaymasterParams {
   collector: Collector;
   masterPrivateKey: Hex;
   receiverAddress: Address;
@@ -10,8 +10,28 @@ export interface ConstructParams {
   cellAmount: number;
 }
 
-export interface RgbppTransferParams {
-  collector: Collector;
-  xudtType: Hex;
+export interface BtcRgbppTransfer {
   transferAmount: bigint;
+  btcAddress: Address;
+}
+
+export interface RgbppL1TransferVirtualParams {
+  collector: Collector;
+  xudtTypeBytes: Hex;
+  rgbppLockArgsList: Hex[];
+  transferAmount: bigint;
+  isMainnet?: boolean;
+}
+
+export interface RgbppL1TransferVirtualTx {
+  inputs: CKBComponents.CellInput[];
+  outputs: CKBComponents.CellOutput[];
+  outputsData: Hex[];
+}
+
+export interface RgbppL1TransferVirtualResult {
+  ckbRawTx: CKBComponents.RawTransaction;
+  commitment: Hex;
+  needPaymasterCell: boolean;
+  sumInputsCapacity: bigint;
 }
