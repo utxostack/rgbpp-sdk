720969075851411eb99d1cda45885328c5278e49
Dylan Duan <duanyytop@gmail.com>
2024-03-11 15:27:39 +0800

    tests: add tests for ckb-tx utils

---
+++ packages/ckb/src/utils/ckb-tx.spec.ts
@@ -0,0 +1,15 @@
+import { describe, it, expect } from 'vitest';
+import { leToU128, u128ToLe, u32ToLe, u64ToLe } from './hex';
+import { calculateRgbppCellCapacity } from './ckb-tx';
+
+describe('ckb tx utils', () => {
+  it('calculateRgbppCellCapacity', async () => {
+    const xudtType: CKBComponents.Script = {
+      codeHash: '0x25c29dc317811a6f6f3985a7a9ebc4838bd388d19d0feeecf0bcd60f6c0975bb',
+      hashType: 'type',
+      args: '0x06ec22c2def100bba3e295a1ff279c490d227151bf3166a4f3f008906c849399',
+    };
+    const capacity = calculateRgbppCellCapacity(xudtType);
+    expect(BigInt(159_0000_0000)).toBe(capacity);
+  });
+});
