5c280f279831bab64b6794888f0faf3f926c40e1
Dylan Duan <duanyytop@gmail.com>
2024-03-18 11:54:22 +0800

    fix: Update rgbpp tx state

--- packages/ckb/src/types/queue.ts
+++ packages/ckb/src/types/queue.ts
@@ -10,7 +10,15 @@ export interface AppendRgppTxResp {
   state: string;
 }
 
+/**
+ * completed: The CKB transaction has been sent and confirmed.
+ * failed: Something went wrong during the process, and it has failed.
+ * delayed: The transaction has not been confirmed yet and is waiting for confirmation.
+ * active: The transaction is currently being processed.
+ * waiting: The transaction is pending and is waiting to be processed.
+ */
+export type RgbppTxState = 'completed' | 'failed' | 'delayed' | 'active' | 'waiting';
 export interface CheckRgppTxResp {
   ckbTxHash: string;
-  state: Hex;
+  state: RgbppTxState;
 }
