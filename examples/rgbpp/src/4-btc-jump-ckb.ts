e6ce0016f2bcd83750e9ab736bdf384a38af347e
Dylan Duan <duanyytop@gmail.com>
2024-03-19 17:44:55 +0800

    feat: add jumping from BTC to CKB example

---
+++ examples/rgbpp/src/4-btc-jump-ckb.ts
@@ -0,0 +1,93 @@
+import { AddressPrefix, addressToScript, privateKeyToAddress, serializeScript } from '@nervosnetwork/ckb-sdk-utils';
+import { Collector, SPVService, appendCkbTxWitnesses, appendPaymasterCellAndSignCkbTx, genBtcJumpCkbVirtualTx, sendCkbTx, updateCkbTxWithRealBtcTxId } from '@rgbpp-sdk/ckb';
+import { sendRgbppUtxos, BtcAssetsApi, DataSource, NetworkType } from '@rgbpp-sdk/btc';
+
+// SECP256K1 private key
+const TEST_PRIVATE_KEY = '0x0000000000000000000000000000000000000000000000000000000000000001';
+
+interface Params {
+  signer: any
+  rgbppLockArgsList: string[];
+  toCkbAddress: string;
+  transferAmount: bigint;
+}
+const jumpFromBtcToCkb = async ({ signer, rgbppLockArgsList, toCkbAddress, transferAmount }: Params) => {
+  const collector = new Collector({
+    ckbNodeUrl: 'https://testnet.ckb.dev/rpc',
+    ckbIndexerUrl: 'https://testnet.ckb.dev/indexer',
+  });
+  const address = privateKeyToAddress(TEST_PRIVATE_KEY, { prefix: AddressPrefix.Testnet });
+  console.log('address: ', address);
+  const fromLock = addressToScript(address);
+
+  const networkType = NetworkType.TESTNET;
+  // TODO: Use the real btc_assets_api_url and token
+  const service = BtcAssetsApi.fromToken('btc_assets_api_url', 'your_token');
+  const source = new DataSource(service, networkType);
+
+  // TODO: Use the real XUDT type script
+  const xudtType: CKBComponents.Script = {
+    codeHash: '0x25c29dc317811a6f6f3985a7a9ebc4838bd388d19d0feeecf0bcd60f6c0975bb',
+    hashType: 'type',
+    args: '0x1ba116c119d1cfd98a53e9d1a615cf2af2bb87d95515c9d217d367054cfc696b',
+  };
+
+  const ckbVirtualTxResult = await genBtcJumpCkbVirtualTx({
+    collector,
+    rgbppLockArgsList,
+    xudtTypeBytes: serializeScript(xudtType),
+    transferAmount,
+    toCkbAddress,
+    isMainnet: false,
+  });
+
+  const { commitment, ckbRawTx, needPaymasterCell, sumInputsCapacity } = ckbVirtualTxResult;
+
+  // TODO: call sendRgbppUtxos to build and sign btc tx
+  const { btcTxId, btcTxBytes } = await sendRgbppUtxos({
+    ckbVirtualTx: ckbRawTx,
+    commitment,
+    tos: signer.getAddress(),
+    signer,
+    source,
+  });
+
+  const newCkbRawTx = updateCkbTxWithRealBtcTxId({ ckbRawTx, btcTxId, isMainnet: false });
+
+  // TODO: Use the real spv-service-url
+  const spvService = new SPVService('spv-service-url');
+
+  let ckbTx = await appendCkbTxWitnesses({
+    ckbRawTx: newCkbRawTx,
+    btcTxBytes,
+    spvService,
+    btcTxId,
+    needPaymasterCell,
+    sumInputsCapacity,
+  });
+
+  if (needPaymasterCell) {
+    const emptyCells = await collector.getCells({ lock: fromLock });
+    if (!emptyCells || emptyCells.length === 0) {
+      throw new Error('The address has no empty cells');
+    }
+    ckbTx = await appendPaymasterCellAndSignCkbTx({
+      secp256k1PrivateKey: TEST_PRIVATE_KEY,
+      ckbRawTx: newCkbRawTx,
+      sumInputsCapacity,
+      paymasterCell: emptyCells[0],
+      isMainnet: false,
+    });
+  }
+
+  await sendCkbTx({ collector, signedTx: ckbTx });
+};
+
+// TODO: Use real btc utxo information
+// rgbppLockArgs: outIndexU32 + btcTxId
+jumpFromBtcToCkb({
+  signer: '',
+  rgbppLockArgsList: ['0x0100000047448104a611ecb16ab8d8e500b2166689612c93fc7ef18783d8189f3079f447'],
+  toCkbAddress: 'ckt1qrfrwcdnvssswdwpn3s9v8fp87emat306ctjwsm3nmlkjg8qyza2cqgqq9kxr7vy7yknezj0vj0xptx6thk6pwyr0sxamv6q',
+  transferAmount: BigInt(100_0000_0000),
+});
