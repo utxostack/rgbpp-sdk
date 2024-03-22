22522ce16fc360f4bdd8e6a12cb3cd09c2d176f3
Dylan Duan <duanyytop@gmail.com>
2024-03-22 16:50:50 +0800

    fix rgbpp cells length

--- packages/ckb/src/rgbpp/btc-transfer.ts
+++ packages/ckb/src/rgbpp/btc-transfer.ts
@@ -35,7 +35,7 @@ export const genBtcTransferCkbVirtualTx = async ({
   rgbppCells = rgbppCells.sort(compareInputs);
 
   const { inputs, sumInputsCapacity, sumAmount } = collector.collectUdtInputs(rgbppCells, transferAmount);
-  rgbppCells = rgbppCells.slice(inputs.length);
+  rgbppCells = rgbppCells.slice(0, inputs.length);
 
   const rpbppCellCapacity = calculateRgbppCellCapacity(xudtType);
   const outputsData = [append0x(u128ToLe(transferAmount))];
