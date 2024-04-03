7f349742decdd83d1e641429d4584970e3e8d2aa
Dylan Duan <duanyytop@gmail.com>
2024-04-04 03:51:00 +0800

    refactor: update btc sendRgbppUtxos

--- packages/btc/src/api/sendRgbppUtxos.ts
+++ packages/btc/src/api/sendRgbppUtxos.ts
@@ -1,4 +1,4 @@
-import { Collector, isRgbppLockCell, isBtcTimeLockCell, calculateCommitment } from '@rgbpp-sdk/ckb';
+import { Collector, isRgbppLockCell, calculateCommitment } from '@rgbpp-sdk/ckb';
 import { bitcoin } from '../bitcoin';
 import { Utxo } from '../transaction/utxo';
 import { DataSource } from '../query/source';
@@ -34,8 +34,6 @@ export async function sendRgbppUtxosBuilder(props: SendRgbppUtxosProps): Promise
 }> {
   const btcInputs: Utxo[] = [];
   const btcOutputs: InitOutput[] = [];
-  let lastCkbTypeInputIndex = -1;
-  let lastCkbTypeOutputIndex = -1;
 
   const ckbVirtualTx = props.ckbVirtualTx;
   const config = networkTypeToConfig(props.source.networkType);
@@ -47,16 +45,6 @@ export async function sendRgbppUtxosBuilder(props: SendRgbppUtxosProps): Promise
 
     const ckbLiveCell = await props.ckbCollector.getLiveCell(ckbInput.previousOutput!);
     const isRgbppLock = isRgbppLockCell(ckbLiveCell.output, isCkbMainnet);
-
-    // If input.type !== null, input.lock must be RgbppLock or RgbppTimeLock
-    if (ckbLiveCell.output.type) {
-      if (!isRgbppLock) {
-        throw new TxBuildError(ErrorCodes.CKB_INVALID_CELL_LOCK);
-      }
-
-      // If input.type !== null，update lastTypeInput
-      lastCkbTypeInputIndex = i;
-    }
 
     // If input.lock == RgbppLock, add to inputs if:
     // 1. input.lock.args can be unpacked to RgbppLockArgs
@@ -97,17 +85,6 @@ export async function sendRgbppUtxosBuilder(props: SendRgbppUtxosProps): Promise
   for (let i = 0; i < ckbVirtualTx.outputs.length; i++) {
     const ckbOutput = ckbVirtualTx.outputs[i];
     const isRgbppLock = isRgbppLockCell(ckbOutput, isCkbMainnet);
-    const isBtcTimeLock = isBtcTimeLockCell(ckbOutput, isCkbMainnet);
-
-    // If output.type !== null, then the output.lock must be RgbppLock or RgbppTimeLock
-    if (ckbOutput.type) {
-      if (!isRgbppLock && !isBtcTimeLock) {
-        throw new TxBuildError(ErrorCodes.CKB_INVALID_CELL_LOCK);
-      }
-
-      // If output.type !== null，update lastTypeInput
-      lastCkbTypeOutputIndex = i;
-    }
 
     // If output.lock == RgbppLock, generate a corresponding output in outputs
     if (isRgbppLock) {
@@ -122,17 +99,9 @@ export async function sendRgbppUtxosBuilder(props: SendRgbppUtxosProps): Promise
     }
   }
 
-  // By rules, the length of type outputs should be >= 1
-  // The "lastTypeOutputIndex" is -1 by default so if (index < 0) it's invalid
-  if (lastCkbTypeOutputIndex < 0) {
-    throw new TxBuildError(ErrorCodes.CKB_INVALID_OUTPUTS);
-  }
-
   // Verify the provided commitment
   const calculatedCommitment = calculateCommitment({
-    inputs: [...ckbVirtualTx.inputs].slice(0, lastCkbTypeInputIndex + 1),
-    outputs: [...ckbVirtualTx.outputs].slice(0, lastCkbTypeOutputIndex + 1),
-    outputsData: [...ckbVirtualTx.outputsData].slice(0, lastCkbTypeOutputIndex + 1),
+    ...ckbVirtualTx,
   });
   if (props.commitment !== calculatedCommitment) {
     throw new TxBuildError(ErrorCodes.CKB_UNMATCHED_COMMITMENT);
