b783915d0f837052c40502933c1ce5bbb04aab8d
Dylan Duan <duanyytop@gmail.com>
2024-04-09 14:42:24 +0800

    refactor: Update ckbTxFromTxSkeleton

--- packages/ckb/src/utils/spore.ts
+++ packages/ckb/src/utils/spore.ts
@@ -1,17 +1,5 @@
-import { TransactionSkeletonType } from '@ckb-lumos/helpers';
+import { TransactionSkeletonType, createTransactionFromSkeleton } from '@ckb-lumos/helpers';
 
 export const ckbTxFromTxSkeleton = (txSkeleton: TransactionSkeletonType): CKBComponents.RawTransaction => {
-  const ckbTx: CKBComponents.RawTransaction = {
-    version: '0x',
-    headerDeps: txSkeleton.headerDeps.toArray(),
-    cellDeps: txSkeleton.cellDeps.toArray(),
-    inputs: txSkeleton.inputs.toArray().map((cell) => ({
-      previousOutput: cell.outPoint!,
-      since: '0x0',
-    })),
-    outputs: txSkeleton.outputs.toArray().map((cell) => cell.cellOutput),
-    outputsData: txSkeleton.outputs.toArray().map((cell) => cell.data),
-    witnesses: txSkeleton.witnesses.toArray(),
-  };
-  return ckbTx;
+  return createTransactionFromSkeleton(txSkeleton) as CKBComponents.RawTransaction;
 };
