5fa963a04f2516f4576163e236ae59d3ebfab379
xcshuan <xcshuan@hotmail.com>
2024-04-11 17:09:51 +0800

    add extra attribute for tokenInfo

--- examples/rgbpp/xudt/0-token-info.ts
+++ examples/rgbpp/xudt/0-token-info.ts
@@ -3,5 +3,6 @@ import { RgbppTokenInfo } from "@rgbpp-sdk/ckb";
 export const XUDT_TOKEN_INFO: RgbppTokenInfo = {
   decimal: 8,
   name: 'XUDT Test Token',
-  symbol: 'XTT'
+  symbol: 'XTT',
+  extra: '0x'
 }
