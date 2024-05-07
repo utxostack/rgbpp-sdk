e05c63c138bdf45e888115451485d18b6cc5c21b
Dylan Duan <duanyytop@gmail.com>
2024-05-07 14:48:43 +0800

    feat: Update service packing with vite and rollup

---
+++ packages/service/vite.config.ts
@@ -0,0 +1,21 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'rgbpp-service',
+      // the proper extensions will be added
+      fileName: 'index',
+      formats: ['es', 'umd', 'cjs'],
+    },
+    minify: false,
+    rollupOptions: {
+      external: ['node-fetch', 'crypto'],
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+    },
+  },
+});
