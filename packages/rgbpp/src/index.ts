816ff05590b6e3f8a80d3c2e2571579f0f6d9076
Dylan Duan <duanyytop@gmail.com>
2024-05-09 17:30:35 +0800

    feat: Add sub path for rgbpp package

--- packages/rgbpp/src/index.ts
+++ packages/rgbpp/src/index.ts
@@ -1,7 +1,35 @@
-export { remove0x } from '@rgbpp-sdk/ckb';
-export { ErrorCodes, ErrorMessages } from '@rgbpp-sdk/btc';
-export { ErrorCodes as ServiceErrorCodes, ErrorMessages as ServiceErrorMessage } from '@rgbpp-sdk/service';
+export {
+  genCreateClusterCkbVirtualTx,
+  genCreateSporeCkbVirtualTx,
+  genLeapSporeFromBtcToCkbVirtualTx,
+  genTransferSporeCkbVirtualTx,
+  genBtcTransferCkbVirtualTx,
+  genCkbJumpBtcVirtualTx,
+  genBtcBatchTransferCkbVirtualTx,
+  genLeapSporeFromCkbToBtcRawTx,
+  genRgbppLaunchCkbVirtualTx,
+  genBtcJumpCkbVirtualTx,
+  buildBtcTimeCellsSpentTx,
+  buildSporeBtcTimeCellsSpentTx,
+  signBtcTimeCellSpentTx,
+} from '@rgbpp-sdk/ckb';
+export type {
+  CreateClusterCkbVirtualTxParams,
+  CreateSporeCkbVirtualTxParams,
+  LeapSporeFromBtcToCkbVirtualTxParams,
+  TransferSporeCkbVirtualTxParams,
+  BtcTransferVirtualTxParams,
+  BtcJumpCkbVirtualTxParams,
+  BtcBatchTransferVirtualTxParams,
+  CkbJumpBtcVirtualTxParams,
+  SporeCreateVirtualTxResult,
+  BtcTransferVirtualTxResult,
+  BtcJumpCkbVirtualTxResult,
+  BtcBatchTransferVirtualTxResult,
+  SporeTransferVirtualTxResult,
+  SporeLeapVirtualTxResult,
+  SporeVirtualTxResult,
+} from '@rgbpp-sdk/ckb';
 
-export * from '@rgbpp-sdk/ckb';
-export * from '@rgbpp-sdk/service';
-export * from '@rgbpp-sdk/btc';
+export { BtcAssetsApi } from '@rgbpp-sdk/service';
+export { sendBtc, sendUtxos, sendRgbppUtxos } from '@rgbpp-sdk/btc';
