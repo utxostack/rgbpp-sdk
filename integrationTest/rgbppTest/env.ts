7933770740f5758830a562fa13faf83639136845
Dawn <13477199767@163.com>
2024-05-14 17:15:23 +0800

    test: Create integration tests

---
+++ integrationTest/rgbppTest/env.ts
@@ -0,0 +1,34 @@
+import { AddressPrefix, privateKeyToAddress } from '@nervosnetwork/ckb-sdk-utils';
+import { DataSource, BtcAssetsApi } from 'rgbpp';
+import { ECPair, ECPairInterface, bitcoin, NetworkType } from 'rgbpp/btc';
+import dotenv from 'dotenv';
+import { Collector } from 'rgbpp/ckb';
+
+dotenv.config({ path: __dirname + '/.env' });
+
+export const isMainnet = false
+
+export const collector = new Collector({
+  ckbNodeUrl: 'https://testnet.ckb.dev/rpc',
+  ckbIndexerUrl: 'https://testnet.ckb.dev/indexer',
+});
+export const CKB_PRIVATE_KEY = process.env.INTEGRATION_CKB_PRIVATE_KEY!;
+export const ckbAddress = privateKeyToAddress(CKB_PRIVATE_KEY, {
+  prefix: isMainnet ? AddressPrefix.Mainnet : AddressPrefix.Testnet,
+});
+
+export const BTC_PRIVATE_KEY = process.env.INTEGRATION_BTC_PRIVATE_KEY!;
+export const BTC_SERVICE_URL = process.env.VITE_SERVICE_URL!;
+export const BTC_SERVICE_TOKEN = process.env.VITE_SERVICE_TOKEN!;
+export const BTC_SERVICE_ORIGIN = process.env.VITE_SERVICE_ORIGIN!;
+
+const network = isMainnet ? bitcoin.networks.bitcoin : bitcoin.networks.testnet;
+export const btcKeyPair: ECPairInterface = ECPair.fromPrivateKey(Buffer.from(BTC_PRIVATE_KEY, 'hex'), { network });
+export const { address: btcAddress } = bitcoin.payments.p2wpkh({
+  pubkey: btcKeyPair.publicKey,
+  network,
+});
+
+const networkType = isMainnet ? NetworkType.MAINNET : NetworkType.TESTNET;
+export const btcService = BtcAssetsApi.fromToken(BTC_SERVICE_URL, BTC_SERVICE_TOKEN, BTC_SERVICE_ORIGIN);
+export const btcDataSource = new DataSource(btcService, networkType);
---
+++ integrationTest/rgbppTest/shared/utils.ts
@@ -0,0 +1,26 @@
+import 'dotenv/config';
+import * as fs from 'fs';
+import * as path from 'path';
+import { btcService } from '../env';
+export const network = 'testnet';
+export async function getFastestFeeRate() {
+  // const { service } = getDeployVariables();
+
+  const fees = await btcService.getBtcRecommendedFeeRates();
+  return Math.ceil(fees.fastestFee * 10);
+}
+
+export async function writeStepLog(step: string, data: string | object) {
+  const file = path.join(__dirname, `../${network}/step-${step}.log`);
+
+  if (typeof data !== 'string') {
+    data = JSON.stringify(data);
+  }
+
+  fs.writeFileSync(file, data);
+}
+
+export function readStepLog(step: string) {
+  const file = path.join(__dirname, `../${network}/step-${step}.log`);
+  return JSON.parse(fs.readFileSync(file).toString());
+}
