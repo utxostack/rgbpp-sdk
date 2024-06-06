ba84dc7d09ed4010fbecce92d627320c46788eff
ahonn <yuexunjiang@gmail.com>
2024-06-06 19:07:23 +1000

    feat: use fastify instead of express

--- apps/service/src/main.ts
+++ apps/service/src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 
 async function bootstrap() {
   const logger = new Logger('AppBootstrap');
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
   await app.listen(3000);
   logger.log('Application is running on: http://localhost:3000');
 }
