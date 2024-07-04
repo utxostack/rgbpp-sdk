5f42b04a0485d77d4c1621a0b010af4141038c6b
Shook <shooklyngs@gmail.com>
2024-07-05 01:17:51 +0800

    temp

--- packages/btc/tsup.config.ts
+++ packages/btc/tsup.config.ts
@@ -5,12 +5,8 @@ export default defineConfig({
   name,
   dts: true,
   clean: true,
-  bundle: true,
   sourcemap: true,
-  splitting: true,
-  cjsInterop: true,
   target: 'esnext',
-  platform: 'neutral',
   format: ['esm', 'cjs'],
   entry: ['src/index.ts'],
 });
