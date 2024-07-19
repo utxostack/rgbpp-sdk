73601cb3b56121ed0342b7caad487c34cce0aa1e
Dylan Duan <duanyytop@gmail.com>
2024-07-19 15:32:31 +0800

    feat: Custom btc confirmation blocks for btc time lock

--- packages/ckb/src/utils/rgbpp.ts
+++ packages/ckb/src/utils/rgbpp.ts
@@ -45,6 +45,7 @@ export const genBtcTimeLockArgs = (lock: CKBComponents.Script, btcTxId: Hex, aft
 };
 
 /**
+ * btcTimeLockArgs: 
  * table BTCTimeLock {
     lock_script: Script,
     after: Uint32,
@@ -55,8 +56,13 @@ export const genBtcTimeLockScript = (
   toLock: CKBComponents.Script,
   isMainnet: boolean,
   btcTestnetType?: BTCTestnetType,
+  btcConfirmationBlocks?: number,
 ) => {
-  const args = genBtcTimeLockArgs(toLock, RGBPP_TX_ID_PLACEHOLDER, BTC_JUMP_CONFIRMATION_BLOCKS);
+  const args = genBtcTimeLockArgs(
+    toLock,
+    RGBPP_TX_ID_PLACEHOLDER,
+    btcConfirmationBlocks ?? BTC_JUMP_CONFIRMATION_BLOCKS,
+  );
   return {
     ...getBtcTimeLockScript(isMainnet, btcTestnetType),
     args,
